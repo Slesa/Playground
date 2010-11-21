1ac6e42096ee97560e98a08c867c179783f50556
slesa <joerg.preiss@me.com>
2010-11-21 22:06:56 +0100

    Adding F# example PieChart

---
+++ sketches/fsharp/PieChart/PieChart/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let convertDataRow(csvLine:string) =
+    let cells = List.ofSeq(csvLine.Split(','))
+    match cells with
+    | title::number::_ ->
+        let parsedNumber = Int32.Parse(number)
+        (title, parsedNumber)
+    | _ -> failwith "Incorrect data format!"
+
+let x = convertDataRow("title,123")
+
