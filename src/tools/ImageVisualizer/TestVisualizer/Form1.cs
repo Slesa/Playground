0d28f0e5d0fde558cfe18c42961d7af6430b06f9
Slesa <joerg.preiss@me.com>
2010-11-22 23:24:53 +0100

    Added Image Visualizer for VS2010 Debugger

---
+++ src/tools/ImageVisualizer/TestVisualizer/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog { Filter = "JPG files|*.jpg|PNG Files|*.png" };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var bitmap = new Bitmap(dialog.FileName);
+                pictureBox.Image = bitmap;
+            }
+        }
+    }
+}
