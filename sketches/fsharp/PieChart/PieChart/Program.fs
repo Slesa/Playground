ae2cde6af0f2a2186d9b7c46300cc48299085fca
Slesa <joerg.preiss@me.com>
2010-11-22 11:15:20 +0100

    FSharp pie chart drawing finished

--- sketches/fsharp/PieChart/PieChart/Program.fs
+++ sketches/fsharp/PieChart/PieChart/Program.fs
@@ -1,12 +1,37 @@
-﻿open System
+﻿module Program
 
-let convertDataRow(csvLine:string) =
-    let cells = List.ofSeq(csvLine.Split(','))
-    match cells with
-    | title::number::_ ->
-        let parsedNumber = Int32.Parse(number)
-        (title, parsedNumber)
-    | _ -> failwith "Incorrect data format!"
+open System
+open System.Drawing
+open System.Windows.Forms
+open Drawings
 
-let x = convertDataRow("title,123")
+let mainForm = new Form(Width=620, Height=450, Text="Pie Chart")
 
+let menu = new ToolStrip()
+let btnOpen = new ToolStripButton("Open")
+let btnSave = new ToolStripButton("Save", Enabled=false)
+ignore(menu.Items.Add(btnOpen))
+ignore(menu.Items.Add(btnSave))
+
+let boxChart = new PictureBox(BackColor=Color.White, Dock=DockStyle.Fill, SizeMode=PictureBoxSizeMode.CenterImage)
+
+mainForm.Controls.Add(menu)
+mainForm.Controls.Add(boxChart)
+
+let openAndDrawChart(e) = 
+    let dlg = new OpenFileDialog(Filter="CSV Files|*.csv")
+    if( dlg.ShowDialog() = DialogResult.OK) then
+        let pieChart = Drawings.drawChart(dlg.FileName)
+        boxChart.Image <- pieChart
+        btnSave.Enabled <- true
+
+let saveDrawing(e) = 
+    let dlg = new SaveFileDialog(Filter="PNG Files|*.png")
+    if( dlg.ShowDialog() = DialogResult.OK) then
+        boxChart.Image.Save(dlg.FileName)
+
+[<STAThread>]
+do
+    btnOpen.Click.Add(openAndDrawChart)
+    btnSave.Click.Add(saveDrawing)
+    Application.Run(mainForm)
