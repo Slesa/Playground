e32ba733d590181a6aab04024921fb78b4624b94
U-aubawork\auba <joerg.preiss@me.com>
2011-02-16 14:46:57 +0100

    WPF Night hawk does not run, so added SL project. Does not run either.

--- sketches/Caliburn.Micro/NightHawk/NightHawk/MefBootstrapper.cs
+++ sketches/Caliburn.Micro/NightHawk/NightHawk/MefBootstrapper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
 using System.Linq;
 using System.Reflection;
 using Caliburn.Micro;
--- sketches/Caliburn.Micro/NightHawk/NightHawk/Resources/AppStrings.Designer.cs
+++ sketches/Caliburn.Micro/NightHawk/NightHawk/Resources/AppStrings.Designer.cs
@@ -22,7 +22,7 @@ namespace NightHawk.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class AppStrings {
+    public class AppStrings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace NightHawk.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NightHawk.Resources.AppStrings", typeof(AppStrings).Assembly);
@@ -51,7 +51,7 @@ namespace NightHawk.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace NightHawk.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Night Hawk.
         /// </summary>
-        internal static string AppTitle {
+        public static string AppTitle {
             get {
                 return ResourceManager.GetString("AppTitle", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace NightHawk.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Ok.
         /// </summary>
-        internal static string OkButtonText {
+        public static string OkButtonText {
             get {
                 return ResourceManager.GetString("OkButtonText", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace NightHawk.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Error.
         /// </summary>
-        internal static string StringError {
+        public static string StringError {
             get {
                 return ResourceManager.GetString("StringError", resourceCulture);
             }
