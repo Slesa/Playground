571aebffcf64881e7360f98547eab617de460548
Auba <joerg.preiss@me.com>
2011-05-03 23:05:12 +0200

    Added Busy Indicators to modules view. Inserted FocusManager hints in Xamls

--- src/Lucifer/Lucifer.Pms.Editor/View/ListSalesItemsView.xaml.cs
+++ src/Lucifer/Lucifer.Pms.Editor/View/ListSalesItemsView.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Lucifer.Pms.Editor.View
+﻿namespace Lucifer.Pms.Editor.View
 {
     /// <summary>
     /// Interaction logic for ListSalesItemView.xaml
     /// </summary>
-    public partial class ListSalesItemsView : UserControl
+    public partial class ListSalesItemsView 
     {
         public ListSalesItemsView()
         {
