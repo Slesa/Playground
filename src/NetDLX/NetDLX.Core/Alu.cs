08e14af62aca01d248ac21e7ba9b3d043d5bfe70
slesa <joerg.preiss@me.com>
2011-06-03 19:54:54 +0200

    Added Arithmetic logic unit

---
+++ src/NetDLX/NetDLX.Core/Alu.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace NetDLX.Core
+{
+    public class Alu
+    {
+        public UInt32 Add(UInt32 a, UInt32 b)
+        {
+            return 0;
+        }
+
+        public UInt32 Sub(UInt32 a, UInt32 b)
+        {
+            return 0;
+        }
+
+        public float Mult(float a, float b)
+        {
+            return 0;
+        }
+
+        public float Div(float a, float b)
+        {
+            return 0;
+        }
+
+        public double Mult(double a, double b)
+        {
+            return 0;
+        }
+
+        public double Div(double a, double b)
+        {
+            return 0;
+        }
+
+        public UInt32 And(UInt32 a, UInt32 b)
+        {
+            return 0;
+        }
+
+        public UInt32 Or(UInt32 a, UInt32 b)
+        {
+            return 0;
+        }
+
+        public UInt32 Xor(UInt32 a, UInt32 b)
+        {
+            return 0;
+        }
+
+        public UInt32 Lhh(UInt16 value)
+        {
+            return 0;
+        }
+
+        public UInt32 ShiftLeft(UInt32 value)
+        {
+            return 0;
+        }
+
+        public UInt32 ShiftLogicLeft(UInt32 value)
+        {
+            return 0;
+        }
+
+        public UInt32 ShiftRight(UInt32 value)
+        {
+            return 0;
+        }
+
+        public UInt32 ShiftLogicRight(UInt32 value)
+        {
+            return 0;
+        }
+    }
+}
