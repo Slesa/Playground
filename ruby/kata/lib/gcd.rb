fac3a1a7c11c1ff5b090b90a9a86ad6c156be64e
Jörg Preiß <joerg.preiss@slesa.de>
2011-07-20 10:07:23 +0200

    The missing link from agross: return must be used in one-line-ifs

--- ruby/kata/lib/gcd.rb
+++ ruby/kata/lib/gcd.rb
@@ -3,18 +3,7 @@
 # gcd(b, a mod b)  -> Euklid's algorithm
 
 def gcd(a,b)
-	#puts "a=#{a}, b=#{b}"
-	if b==0 then 
-		#puts "b is 0"
-		a
-	else
-		#a if b==0
-		#puts "b is obviously not 0"
-		if a%b!=0 then 
-			gcd(b, a%b)
-		else
-	#gcd(b, a%b) unless a%b!=0
-			b
-		end
-	end
+	return a if b==0
+	return b if a%b==0
+	gcd(b, a%b)
 end
