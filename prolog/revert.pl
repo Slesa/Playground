fa256970ec0a6459afb9adf0c3f8686849318400
Auba <joerg.preiss@me.com>
2011-08-22 21:34:39 +0200

    First unit test in prolog

--- prolog/revert.pl
+++ prolog/revert.pl
@@ -1,2 +1,15 @@
 revert([], []).
-revert([Head|Tail], List) :- revert(Tail, What), append(What, Head, List).
+revert([Head|Tail], Reversed) :-
+	revert(Tail, List),
+	append(List, [Head], Reversed).
+
+:- use_module(library(plunit)).
+
+:- begin_tests(revert).
+
+test(revert_empty) :- revert([], []).
+test(revert_two) :- revert([a, b], [b, a]).
+test(revert_three) :- revert([a, b, c], [c, b, a]).
+
+:- end_tests(revert).
+
