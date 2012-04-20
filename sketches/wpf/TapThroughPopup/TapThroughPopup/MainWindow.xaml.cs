c586ec9b37094804ea6352c49df5d0c0e286ad2d
J. Preiss <joerg.preiss@me.com>
2012-04-20 18:00:47 +0200

    Added TapThroughBehavior, tap through a combobox

---
+++ sketches/wpf/TapThroughPopup/TapThroughPopup/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace TapThroughPopup
+{
+    public partial class MainWindow : Window 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Background = Brushes.BlanchedAlmond;
+        }
+
+        void ChangeBackground(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Background = Background == Brushes.BlanchedAlmond ? Brushes.Aquamarine : Brushes.BlanchedAlmond;
+        }
+        
+    }
+}
---
+++ sketches/wpf/TapThroughPopup/TapThroughPopup/TapThroughBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace TapThroughPopup
+{
+    class TapThroughBehavior : Behavior<ComboBox>
+    {
+        protected override void OnAttached()
+        {
+            Mouse.AddPreviewMouseDownOutsideCapturedElementHandler(AssociatedObject, MouseDownOutsideCapturedElement);
+        }
+
+        void MouseDownOutsideCapturedElement(object sender, MouseButtonEventArgs e)
+        {
+            AssociatedObject.ReleaseMouseCapture();
+        }
+    }
+}
