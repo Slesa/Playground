ddcd9479116631b4743d8908da4944de51ae055a
J. Preiss <joerg.preiss@me.com>
2012-11-17 00:14:46 +0100

    TreeView selection

--- sketches/wpf/Selections/Selections/ViewModels/TreeViewModel.cs
+++ sketches/wpf/Selections/Selections/ViewModels/TreeViewModel.cs
@@ -1,17 +1,48 @@
 ﻿using System.Collections.ObjectModel;
+using Microsoft.Practices.Prism.ViewModel;
 
 namespace Selections.ViewModels
 {
-    public class TreeViewModel
+    public class TreeViewModel : NotificationObject
     {
         ObservableCollection<NodeViewModel> _nodes;
 
         public ObservableCollection<NodeViewModel> Nodes
         {
-            get { return _nodes ?? (_nodes = CollectionCreator.CreateNodes(200, 5)); }
+            get { return _nodes ?? (_nodes = CollectionCreator.CreateNodes(100, 5)); }
         }
 
-        public NodeViewModel SelectedNode { get; set; }
-        public ItemViewModel SelectedItem { get; set; }
+        NodeViewModel _selectedNode;
+        public NodeViewModel SelectedNode
+        {
+            get { return _selectedNode; }
+            set
+            {
+                _selectedNode = value;
+                RaisePropertyChanged(() => SelectedNode);
+            }
+        }
+
+        ItemViewModel _selectedLeaf;
+        public ItemViewModel SelectedLeaf
+        {
+            get { return _selectedLeaf; }
+            set
+            {
+                _selectedLeaf = value;
+                RaisePropertyChanged(() => SelectedLeaf);
+            }
+        }
+
+        object _selectedItem;
+        public object SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                SelectedLeaf = value as ItemViewModel;
+                SelectedNode = value as NodeViewModel;
+            }
+        }
     }
 }
