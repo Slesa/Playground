5ad5063a21c6505053757e01db2dec4629179154
J. Preiss <joerg.preiss@me.com>
2013-10-15 22:06:09 +0200

    Added itemsPanels

---
+++ sketches/wpf/ItemsPanels/ItemsPanels/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ItemsPanels
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ColorList.ItemsSource = typeof (Colors).GetProperties();
+        }
+
+        ObservableCollection<string> _items;
+
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                if (_items == null)
+                {
+                    _items = new ObservableCollection<string>();
+                    AddItem();
+                    AddItem();
+                    AddItem();
+                }
+                return _items;
+            }
+        }
+
+        void AddItem()
+        {
+            Items.Add(CreateItem());
+        }
+
+        string CreateItem()
+        {
+            return string.Format("Item {0}", Items.Count + 1);
+        }
+
+        void OnAddItem(object sender, RoutedEventArgs e)
+        {
+            AddItem();
+        }
+    }
+}
---
+++ sketches/wpf/ItemsPanels/ItemsPanels/RadialPanel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ItemsPanels
+{
+    public class RadialPanel : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var size = new Size(double.PositiveInfinity, double.PositiveInfinity);
+            
+            foreach (UIElement element in Children)
+            {
+                element.Measure(size);
+            }
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (Children.Count == 0) return finalSize;
+
+            var angle = 0.0;
+            var incrementalAngularSpace = (360.0/Children.Count)*(Math.PI/180.0);
+
+            var radiousX = finalSize.Width/2.4;
+            var radiousY = finalSize.Height/2.4;
+
+            foreach (UIElement element in Children)
+            {
+                var childPoint = new Point(Math.Cos(angle)*radiousX, -Math.Sin(angle)*radiousY);
+                var actualChildPoint = new Point(finalSize.Width/2 + childPoint.X - element.DesiredSize.Width/2,
+                                                 finalSize.Height/2 + childPoint.Y - element.DesiredSize.Height/2);
+                element.Arrange(new Rect(actualChildPoint.X, actualChildPoint.Y, element.DesiredSize.Width, element.DesiredSize.Height));
+                angle += incrementalAngularSpace;
+            }
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
