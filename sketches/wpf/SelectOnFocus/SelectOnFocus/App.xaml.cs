922ccc1a74c2fa342daba43ca2c2a70254d885bd
J. Preiss <joerg.preiss@me.com>
2014-10-14 22:29:23 +0200

    Blogeintrag SelectTextOFocus

---
+++ sketches/wpf/SelectOnFocus/SelectOnFocus/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SelectOnFocus
+{
+    public partial class App : Application
+    {
+        /*
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.GotFocusEvent, new RoutedEventHandler(TextBox_GotFocus));
+            base.OnStartup(e);
+        }
+
+        void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as TextBox;
+            if (tb == null) return;
+            tb.Focus();
+            tb.SelectAll();
+        } 
+         * */
+    }
+}
---
+++ sketches/wpf/SelectOnFocus/SelectOnFocus/SelectTextOnFocus.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace SelectOnFocus
+{
+    public class SelectTextOnFocus :  Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.GotFocus += OnFocus;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.GotFocus -= OnFocus;
+            base.OnDetaching();
+        }
+
+        void OnFocus(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+    }
+}
